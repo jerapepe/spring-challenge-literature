4064e3052eafc33bc81b5e25cdf84cb66b93215e
jerapepe <ruizpepe402@gmail.com>
2024-07-14 17:18:07 -0600

    first commit

---
+++ src/test/java/com/jera/Literatura/LiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jera.Literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
